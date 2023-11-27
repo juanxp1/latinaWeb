99d3f2c07acb0d07809e687af379dd32049df68c
Juan Jose Bianchi <juan-josebianchi@hotmail.com>
2023-11-27 16:45:08 -0300

    formDisable

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -25,55 +25,56 @@ const Contact = ({ title, content, id, t }: ContactProps) => {
   };
 
   return (
-    <ContactContainer id={id} >
-      <p className="bg-warning">Inhabilitado temporalmente el formulario, disculpe la molestias</p>
-      <p className="bg-info">Nos puedes contactar via whatsapp y te responderemos a la brevedad </p>
-      <Row justify="space-between" align="middle">
-        <Col lg={12} md={11} sm={24} xs={24}>
-          <Slide direction="left">
-            <Block title={title} content={content} />
-          </Slide>
-        </Col>
-        <Col lg={12} md={12} sm={24} xs={24}>
-          <Slide direction="right">
-            <FormGroup autoComplete="off" onSubmit={handleSubmit}>
-              <Col span={24}>
-                <Input
-                  type="text"
-                  name="Nombres"
-                  placeholder="Tu nombre"
-                  value={values.name || ""}
-                  onChange={handleChange}
-                />
-                <ValidationType type="name" />
-              </Col>
-              <Col span={24}>
-                <Input
-                  type="text"
-                  name="email"
-                  placeholder="Tu Email"
-                  value={values.email || ""}
-                  onChange={handleChange}
-                />
-                <ValidationType type="email" />
-              </Col>
-              <Col span={24}>
-                <TextArea
-                  placeholder="tu Mensaje"
-                  value={values.message || ""}
-                  name="mensaje"
-                  onChange={handleChange}
-                />
-                <ValidationType type="message" />
-              </Col>
-              <ButtonContainer>
-                <Button name="Enviar">{t("Enviar")}</Button>
-              </ButtonContainer>
-            </FormGroup>
-          </Slide>
-        </Col>
-      </Row>
-    </ContactContainer>
+    <></>
+    // <ContactContainer id={id} >
+    //   <p className="bg-warning">Inhabilitado temporalmente el formulario, disculpe la molestias</p>
+    //   <p className="bg-info">Nos puedes contactar via whatsapp y te responderemos a la brevedad </p>
+    //   <Row justify="space-between" align="middle">
+    //     <Col lg={12} md={11} sm={24} xs={24}>
+    //       <Slide direction="left">
+    //         <Block title={title} content={content} />
+    //       </Slide>
+    //     </Col>
+    //     <Col lg={12} md={12} sm={24} xs={24}>
+    //       <Slide direction="right">
+    //         <FormGroup autoComplete="off" onSubmit={handleSubmit}>
+    //           <Col span={24}>
+    //             <Input
+    //               type="text"
+    //               name="Nombres"
+    //               placeholder="Tu nombre"
+    //               value={values.name || ""}
+    //               onChange={handleChange}
+    //             />
+    //             <ValidationType type="name" />
+    //           </Col>
+    //           <Col span={24}>
+    //             <Input
+    //               type="text"
+    //               name="email"
+    //               placeholder="Tu Email"
+    //               value={values.email || ""}
+    //               onChange={handleChange}
+    //             />
+    //             <ValidationType type="email" />
+    //           </Col>
+    //           <Col span={24}>
+    //             <TextArea
+    //               placeholder="tu Mensaje"
+    //               value={values.message || ""}
+    //               name="mensaje"
+    //               onChange={handleChange}
+    //             />
+    //             <ValidationType type="message" />
+    //           </Col>
+    //           <ButtonContainer>
+    //             <Button name="Enviar">{t("Enviar")}</Button>
+    //           </ButtonContainer>
+    //         </FormGroup>
+    //       </Slide>
+    //     </Col>
+    //   </Row>
+    // </ContactContainer>
   );
 };
 
